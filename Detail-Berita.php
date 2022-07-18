ae7658f4a304537287679ff75eb47aa4f047bd93
GustiRafi <gustirafi49@gmail.com>
2022-07-18 14:28:39 +0700

    membuat tampilan detail berita

---
+++ Detail-Berita.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <title>SMK N 1 BANTUL</title>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top shadow-sm" id="nav">
+        <div class="container">
+            <div class="col d-flex">
+                <img src="img/logo.png" width="70px" height="70px" alt="logo smk 1 Bantul" srcset="">
+                <h2 class="ps-3 " href="#"><strong>SMK NEGERI 1 BANTUL</strong></h2>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
+                aria-expanded="false aria-controls" id="tgl" offcanvasExample">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-start bg-white text-white" tabindex="-1"
+                aria-labelledby="offcanvasExampleLabel" id="offcanvasExample">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title " id="offcanvasExampleLabel">SMK N 1 BANTUL</h5>
+                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"
+                        id="close-tgl"></button>
+                </div>
+                <div class="offcanvas-body text-dark">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="profile.php">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="Guru.php">Guru</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="siswa.php">Siswa</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="perpustakaan.php">Perpustakaan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="BKK.php">BKK</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="galery.php">Galery</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- end Navbar -->
+
+    <!-- main content -->
+    <div class="container mt-3">
+
+        <!-- konten -->
+        <div class="row row-cols-1 row-cols-md-2 g-4 my-3">
+            <div class="col-12 col-md-8">
+                <div class="card shadow">
+                    <div class="px-3 pt-3">
+                        <div data-aos="fade-right">
+                            <div class="d-flex my-3">
+                                <span class="border border-5 border-primary"></span>
+                                <h4 class="ps-3">JUARA LKS DIY 2022, SMK N 1 BANTUL MENUJU LKS TINGKAT NASIONAL</h4>
+                            </div>
+                        </div>
+                        <div data-aos="fade-right">
+                            <p class="opacity-50">Selasa, 31 Mei 2022, administrator</p>
+                        </div>
+                        <div data-aos="fade-right">
+                            <div class="mt-3">
+                                <img src="img/berita3.jpeg" alt="" srcset="">
+                                <p class="pt-3">Senin, 30 Mei 2022, enam siswa SMK N 1 Bantul menerima penghargaan dari
+                                    Dinas Pendidikan
+                                    Pemuda dan Olahraga Provinsi Daerah Istimewa Yogyakarta setelah menjuarai Lomba
+                                    Kompetensi Siswa Tingkat Provinsi. Enam siswa tersebut yaitu Muhammad Khidir Ibrahim
+                                    kelas XI RPL 2 sebagai juara 1 bidang lomba Cyber Security; Muhammad Ma'ruf Illyasa
+                                    kelas XI RPL 2 sebagai juara 1 bidang lomba Web Technology; Alfian Gading Saputra
+                                    kelas
+                                    XI RPL sebagai juara 2 bidang lomba IT Software for Business Solution; Ananda Farhan
+                                    Mega Jiwantoro kelas XII MM 2 sebagai juara 1 bidang lomba 3D Game Art; Alifia
+                                    Divanka
+                                    kelas XI BDP 1 sebagai juara 1 bidang lomba Marketing Online; dan Yusuf Alfahad
+                                    kelas XI
+                                    TKJ 1 sebagai juara 2 bidang lomba Information Network Cabling. Atas prestasinya
+                                    tersebut, keenam siswa menerima tropi dan uang pembinaan.
+
+                                    Kepala Sekolah SMK N 1 Bantul, Bapak Mujari, M.Pd., turut bangga dan mengapresiasi
+                                    prestasi yang telah diraih delegasil LKS DIY 2022. “Saya atas nama sekolah
+                                    mengucapkan
+                                    terima kasih banyak atas perjuangan TIM LKS DIY 2022 sebagai wakil sekolah. Saya
+                                    juga
+                                    sangat bangga mengapresiasi atas capaian prestasi yang telah diraih. Sungguh ini
+                                    menjadi
+                                    kabar yang menggembirakan sebab LKS menjadi salah satu indikator keberhasilan
+                                    sekolah
+                                    setelah UN tidak lagi diselenggarakan. Saya juga berpesan bahwa prestasi ini
+                                    merupakan
+                                    tantangan karena siswa kita akan menjadi wakil Provinsi DIY di tingkat nasional.
+                                    Terakhir, saya berharap prestasi ini dapat menjadi inspirasi dan motivasi bagi siswa
+                                    yang lain”, ujar Kepala Sekolah dalam breifing di ruang Wijaya Kusuma, Selasa 30 Mei
+                                    2022.
+
+                                    Di tempat terpisah, Muhammad Khidir Ibrahim, menyampaikan tanggapan atas prestasi
+                                    yang
+                                    telah diraih. “Saya mengucapkan terima kasih kepada sekolah atas kesempatan dan
+                                    kepercayaan yang diberikan. Saya juga mengucapkan terima kasih kepada Bapak dan Ibu
+                                    Guru
+                                    Pembimbing yang telah sepenuh hati mendampingi proses kompetisi ini” tuturnya.
+                                    Khusus,
+                                    Khidir, prestasi ini bukan yang pertama. Sebelumnya, siswa berperawakan kecil ini
+                                    juga
+                                    berhasil meraih juara 1 LKS DIY 2021 bidang lomba web technologies.
+
+                                    Dalam wawancara terpisah, Khidir berbagi pengalaman atas kemampuannya di bidang web.
+                                    “Saya tertarik dan belajar web sejak SMP secara autodidak. Ditambah ilmu, bimbingan
+                                    guru, dan dukungan SMK N 1 Bantul, saya siap berjuang dan memberikan yang terbaik
+                                    bagi
+                                    DIY di LKS tingkat nasional” tuturnya kepada redaksi. Kisah Khidir dan kelima siswa
+                                    berprestasi ini diharapkan menjadi teladan bagi siswa yang lain untuk berprestasi
+                                    baik
+                                    di bidang akademik mapun nonakademik.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php require_once 'footer.php' ?>
