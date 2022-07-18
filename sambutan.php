43b1c066f98f6f7a223b852799f5d2a697ddc106
GustiRafi <gustirafi49@gmail.com>
2022-07-18 14:28:02 +0700

    membuat halaman sambutan.php

---
+++ sambutan.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <title>SMK N 1 BANTUL</title>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top shadow-sm" id="nav">
+        <div class="container">
+            <div class="col d-flex">
+                <img src="img/logo.png" width="70px" height="70px" alt="logo smk 1 Bantul" srcset="">
+                <h2 class="ps-3 " href="#"><strong>SMK NEGERI 1 BANTUL</strong></h2>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
+                aria-expanded="false aria-controls" id="tgl" offcanvasExample">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-start bg-white text-white" tabindex="-1"
+                aria-labelledby="offcanvasExampleLabel" id="offcanvasExample">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title " id="offcanvasExampleLabel">SMK N 1 BANTUL</h5>
+                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"
+                        id="close-tgl"></button>
+                </div>
+                <div class="offcanvas-body text-dark">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="profile.php">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="Guru.php">Guru</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="siswa.php">Siswa</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="perpustakaan.php">Perpustakaan</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="BKK.php">BKK</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="galery.php">Galery</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- end Navbar -->
+
+    <!-- main content -->
+    <div class="container mt-3">
+
+        <!-- konten -->
+        <div class="row row-cols-1 row-cols-md-2 g-4 my-3">
+            <div class="col-12 col-md-8">
+                <div class="card shadow">
+                    <div class="px-3 pt-3">
+                        <div data-aos="fade-right">
+                            <div class="d-flex my-3">
+                                <span class="border border-5 border-primary"></span>
+                                <h4 class="ps-3">Sambutan Kepala Sekolah</h4>
+                            </div>
+                        </div>
+                        <div data-aos="fade-right">
+                            <div class="mt-3">
+                                <p class="pt-3">Assalamualaikum Wr Wb.</p>
+                                <p>Selamat datang di web SMK Negeri 1 Bantul dengan motto My School Zero Waste Green Is
+                                    Life. Sekolah Saya Indah, Tidak Ada Sampah, Suasana Hijau, Segar Adalah Kehidupanku.
+                                    Silahkan kunjungi web kami. Percayakan siswa siswi, putra putri bapak ibu untuk
+                                    sekolah di SMK Negeri 1 Bantul. Kami siap untuk mengemban amanah dari masyarakat,
+                                    mengantarkan siswa siswi kami menjadi orang yang sukses di masa depan. SMK BISA, SMK
+                                    HEBAT, SMK BISA HEBAT, SMK Negeri 1 Bantul siap untuk mengantarkan putra putri Anda
+                                    di jenjang kesuksesan.
+                                    Terimakasih.</p>
+                                <p>Wassalamualaikum Wr Wb.</p>
+                                <div class="row row-cols-1 float-end">
+                                    <div class="col">
+                                        <p>Kepala SMK N 1 Bantul</p>
+                                        <p>ttd</p>
+                                        <p>Mujari, S.Pd., M.Pd.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php require_once 'footer.php' ?>
